9edae438f9aa60895e1d14eda6beab9946965710
Zhu Liang <zhuliangg11@gmail.com>
2020-12-30 16:20:44 +0800

    feat(ui): updated ui

--- src/App.vue
+++ src/App.vue
@@ -1,39 +1,68 @@
 <template>
   <div id="app">
-    <div>2048 Game</div>
+    <div class="header">
+      2048 Game by <a href="https://github.com/paradite/vue2048">@paradite</a>
+    </div>
     <GameBoard v-bind:game="game" />
   </div>
 </template>
 
 <script>
-import GameBoard from "./components/GameBoard.vue";
-import { Game } from "./game";
+import GameBoard from './components/GameBoard.vue';
+import { Game } from './game';
 
 const game = new Game();
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
-    GameBoard,
+    GameBoard
   },
   mounted() {
-    window.onkeyup = (e) => {
+    window.onkeyup = e => {
       game.handleEvent(e.code);
     };
   },
   data: () => {
     return { game, rows: game.rows };
-  },
+  }
 };
 </script>
 
 <style>
+@font-face {
+  font-family: 'ClearSans';
+  src: url('./../assets/fonts/ClearSans-Bold.woff') format('woff');
+  font-weight: bold;
+  font-style: normal;
+}
+
+@font-face {
+  font-family: 'ClearSans';
+  src: url('./../assets/fonts/ClearSans-Medium.woff') format('woff');
+  font-weight: medium;
+  font-style: normal;
+}
+@font-face {
+  font-family: 'ClearSans';
+  src: url('./../assets/fonts/ClearSans-Regular.woff') format('woff');
+  font-weight: normal;
+  font-style: normal;
+}
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: ClearSans, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+a {
+  color: #2c3e50;
+}
+
+.header {
+  margin-bottom: 4px;
 }
 </style>
--- src/components/Row.vue
+++ src/components/Row.vue
@@ -11,6 +11,7 @@
       v-for="(cell, i) in row"
       :key="i"
       class="cell"
+      v-bind:class="{ [`cell-${cell}`]: true }"
       v-bind:style="{
         width: cellSize + 'px',
         height: cellSize + 'px',
@@ -37,12 +38,56 @@ export default {
 
 <style scoped>
 .cell {
+  color: #f9f6f2;
+  font-weight: bold;
   font-size: 32px;
   display: flex;
   align-items: center;
   justify-content: center;
   float: left;
   background-color: rgba(238, 228, 218, 0.35);
-  border-radius: 5px;
+  border-radius: 2px;
+}
+
+.cell-2 {
+  color: #776e65;
+  background-color: #eee4da;
+}
+
+.cell-4 {
+  color: #776e65;
+  background-color: #eee1c9;
+}
+
+.cell-8 {
+  background: #f3b27a;
+}
+
+.cell-16 {
+  background: #f69664;
+}
+
+.cell-32 {
+  background: #f77c5f;
+}
+
+.cell-64 {
+  background: #f75f3b;
+}
+
+.cell-128 {
+  background: #edd073;
+}
+
+.cell-512 {
+  background: #edc950;
+}
+
+.cell-1024 {
+  background: #edc53f;
+}
+
+.cell-2048 {
+  background: #edc22e;
 }
 </style>
